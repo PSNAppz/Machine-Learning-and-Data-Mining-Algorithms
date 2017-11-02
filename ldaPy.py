dc024ce2bd0657cc3b7627869b13dbc13c39354a
PSN <psn@localhost.localdomain>
2017-11-02 22:54:45 +0530

    Fixed invPooled

--- ldaPy.py
+++ ldaPy.py
@@ -4,16 +4,15 @@ def main():
 	build_rotationtree_model(2)
 
 def lda(newArray):
-
- Curvature = np.array([2.95,2.53,3.57,3.16,2.58,2.16,3.27])  
- Diameter = np.array([6.63,7.79,5.65,5.47,4.46,6.22,3.52])            
+ Curvature = np.array(newArray)[:,0]
+ Diameter = np.array(newArray)[:,1]  
  QCR = np.array([1,1,1,1,0,0,0])
- Mean_X1_A=0
- Mean_X1_B=0
- Mean_X2_A=0
- Mean_X2_B=0
- countX1=0
- countX2=0
+ Mean_X1_A = 0
+ Mean_X1_B = 0
+ Mean_X2_A = 0
+ Mean_X2_B = 0
+ countX1 = 0
+ countX2 = 0
  x=[[0 for x in range(2)] for y in range(len(newArray))]
  x1=[[0 for w in range(2)] for z in range(len(newArray))]
  x2=[[0 for e in range(2)] for q in range(len(newArray))]
@@ -84,8 +83,8 @@ def lda(newArray):
  		pooled[i][j]=(countX1/(countX1+countX2))*CovMat_A[i][j]+(countX2/(countX1+countX2))*CovMat_B[i][j]
  invPooled = [[0 for r in range(len(CovMat_A))] for i in range(len(CovMat_A))]
  invPooled = np.matrix(pooled).I
- print("InvPooled")
- print(invPooled)
+ print (invPooled)
+ return invPooled
 
  
 def build_rotationtree_model(k):
@@ -95,14 +94,17 @@ def build_rotationtree_model(k):
  newArray =[[0 for x in range(k)] for y in range(len(mtx))]
  #Height of matrix(total rows)
  b = mtx.shape[0]
+ #Sparse matrix
+ sparseMat = np.zeros([a,a])
  #Seperation limit
  limit = int(a/k)
  #Starting of sub matrix
  start = 0
  #Ending of sub matrix
  end = int(a/k)
- m=0
- n=0
+ m = 0
+ n = 0
+ init = 0
   #Loop
  while(end <= a):
  	for i in range(0,b):
@@ -111,14 +113,17 @@ def build_rotationtree_model(k):
  			n = 1
  		m=m+1
  		n=0
- 	print(newArray)
- 	lda(newArray)
- 	#Call LDA function and add the result to Sparse Matrix
- 	#sparseMat = LDA(newArray) Should be inside a loop
+ 	invPooled = np.array(lda(newArray))
+ 	for s in range(init,k):
+ 		for t in range(init,k):
+ 			sparseMat[s][t] = invPooled[s][t]
+ 	init = 	init + k	
+ 	print("init",init)
  	m = 0
  	start = end
  	end = end + limit
-
+ print("SPARSE")	
+ print(sparseMat)    
 
 if __name__ == '__main__':
 	main()
