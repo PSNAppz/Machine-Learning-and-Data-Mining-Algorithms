7585072fbe99a9e7f134123e41a493bcc5bc9ff0
PSNAppZ <thepsnarayanan@gmail.com>
2017-11-07 22:54:12 +0530

    fixed sparsematrix

--- backup.py
+++ backup.py
@@ -4,20 +4,21 @@ def main():
 	build_rotationtree_model(2)
 
 def lda(newArray):
- Curvature = np.array(newArray)[:,0]
- Diameter = np.array(newArray)[:,1]  
+
+ Curvature = np.array([2.95,2.53,3.57,3.16,2.58,2.16,3.27])  
+ Diameter = np.array([6.63,7.79,5.65,5.47,4.46,6.22,3.52])            
  QCR = np.array([1,1,1,1,0,0,0])
- Mean_X1_A = 0
- Mean_X1_B = 0
- Mean_X2_A = 0
- Mean_X2_B = 0
- countX1 = 0
- countX2 = 0
- x=[[0 for x in range(2)] for y in range(len(newArray))]
- x1=[[0 for w in range(2)] for z in range(len(newArray))]
- x2=[[0 for e in range(2)] for q in range(len(newArray))]
- y2=[[0 for d in range(2)] for c in range(len(newArray))]
- y2=[[0 for u in range(2)] for y in range(len(newArray))]
+ Mean_X1_A=0
+ Mean_X1_B=0
+ Mean_X2_A=0
+ Mean_X2_B=0
+ countX1=0
+ countX2=0
+ x=[[0 for x in range(2)] for y in range(len(Curvature))]
+ x1=[[0 for w in range(2)] for z in range(len(Curvature))]
+ x2=[[0 for e in range(2)] for q in range(len(Curvature))]
+ y2=[[0 for d in range(2)] for c in range(len(Curvature))]
+ y2=[[0 for u in range(2)] for y in range(len(Curvature))]
  A1=[[0 for v in range(2)] for k in range(4)]
  A2=[[0 for f in range(2)] for j in range(3)]
 
@@ -83,8 +84,8 @@ def lda(newArray):
  		pooled[i][j]=(countX1/(countX1+countX2))*CovMat_A[i][j]+(countX2/(countX1+countX2))*CovMat_B[i][j]
  invPooled = [[0 for r in range(len(CovMat_A))] for i in range(len(CovMat_A))]
  invPooled = np.matrix(pooled).I
- print (invPooled)
- return invPooled
+
+ print(x * invPooled)
 
  
 def build_rotationtree_model(k):
@@ -94,17 +95,14 @@ def build_rotationtree_model(k):
  newArray =[[0 for x in range(k)] for y in range(len(mtx))]
  #Height of matrix(total rows)
  b = mtx.shape[0]
- #Sparse matrix
- sparseMat = np.zeros([a,a])
  #Seperation limit
  limit = int(a/k)
  #Starting of sub matrix
  start = 0
  #Ending of sub matrix
  end = int(a/k)
- m = 0
- n = 0
- pos = 0
+ m=0
+ n=0
   #Loop
  while(end <= a):
  	for i in range(0,b):
@@ -113,37 +111,16 @@ def build_rotationtree_model(k):
  			n = 1
  		m=m+1
  		n=0
- 	invPooled = np.array(lda(newArray))	
- 	sparse(pos,invPooled,k,sparseMat)
- 	pos = pos + k
+ 	print(newArray)
+ 	lda(newArray)
+ 	#Call LDA function and add the result to Sparse Matrix
+ 	#sparseMat = LDA(newArray) Should be inside a loop
  	m = 0
  	start = end
  	end = end + limit
- print("SPARSE")	
- a = np.matrix(mtx)
- b = np.matrix(sparseMat)
- print(a*b)    
 
 
 
-
-def sparse(pos,invPooled,k,sparseMat):
-	e = pos
- 	f = pos
-	init = 0
-	for s in range(init,k):
- 		for t in range(init,k):
- 			sparseMat[e][f] = invPooled[s][t]
- 			print("EF",e,f)
- 			f = f+ 1
- 		e = e + 1
- 		f = pos
- 		print("Break")
- 	init = 	init + k
- 	e = 0
- 	return sparseMat	
-
- 
 
 
 
@@ -151,10 +128,4 @@ if __name__ == '__main__':
 	main()
 
 
-#random subspace
-
-
-
-
-
-#subspacious
+#PSO	
