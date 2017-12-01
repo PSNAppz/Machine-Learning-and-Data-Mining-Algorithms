8678782feb24851b775125dd97852c2720a41f64
PS Narayanan <thepsnarayanan@gmail.com>
2017-12-01 18:46:35 +0530

    Updated Algorithm

--- RotationForest.py
+++ RotationForest.py
@@ -3,7 +3,7 @@ import numpy as np
 from numpy import genfromtxt
 
 def main():
-  build_rotationtree_model(2)
+  build_rotationtree_model(13)
 
 
 
@@ -33,10 +33,15 @@ def LDA(newArray,mtx):
     for j in range(rows):
       if(mtx[j][columns]==1):
         Mean_X1[i] +=x1[j][i]
-        countX1+=1
       else:
         Mean_X2[i]+=x2[j][i]
-        countX2+=1  
+
+  for j in range(rows):
+    if(mtx[j][columns]==1):
+      countX1+=1
+    else:
+      countX2+=1 
+  
   #Global Mean
   for j in range(nCol):
     Mean_X1[j]/= countX1
@@ -51,23 +56,30 @@ def LDA(newArray,mtx):
       else:
         x2[j][i] -= GlobalMean[i]
   A1=[[0 for v in range(nCol)] for k in range(countX1)]
-  A2=[[0 for f in range(nCol)] for j in range(countX2)]     
+  A2=[[0 for f in range(nCol)] for j in range(countX2)] 
+  x=0
+  y=0
   for i in range(rows):
     if(mtx[i][columns]==1):
       for j in range(nCol):
-        A1[i][j] = X[i][j]
+         A1[x][j] = X[i][j]
+      x+=1
     else:
-       for k in range(nCol):
-         A2[i][k] = X[i][k]
-               
+      for k in range(nCol):
+         A2[y][k] = X[i][k]
+      y+=1
   TransMat_A = np.asarray(A1)
   TransMat_B = np.asarray(A2)
-  CovMat_A = np.cov(np.matrix.transpose(TransMat_A))
-  CovMat_B = np.cov(np.matrix.transpose(TransMat_B))     
+  CovMat_A = np.matrix.transpose(TransMat_A) * (np.matrix(TransMat_A)/x)
+  CovMat_B = np.matrix.transpose(TransMat_B) * (np.matrix(TransMat_B)/y)
+  covA=[[0 for f in range(len(CovMat_A))] for j in range(len(CovMat_A))]
+  covB =[[0 for f in range(len(CovMat_B))] for j in range(len(CovMat_B))]
+  covA = np.array(CovMat_A)
+  covB = np.array(CovMat_B)
   pooled =[[0 for f in range(len(CovMat_A))] for j in range(len(CovMat_A))]
   for i in range(len(CovMat_A)):
     for j in range(len(CovMat_A)):
-      pooled[i][j]=(countX1/(countX1+countX2))*CovMat_A[i][j]+(countX2/(countX1+countX2))*CovMat_B[i][j]
+      pooled[i][j] = (countX1/(countX1+countX2))*covA[i][j]+(countX2/(countX1+countX2))*covB[i][j]
   invPooled = [[0 for r in range(len(CovMat_A))] for i in range(len(CovMat_A))]
   invPooled = np.matrix(pooled).I
   return invPooled
@@ -96,19 +108,24 @@ def build_rotationtree_model(k):
   n = 0
   pos = 0
   counter = 0
+  ext = 0
+  add = 0
   #Loop
   while(counter < k):
+      if(counter == k-1):
+        add = a - (k*cond)
+        end = end + add
+        newArray =[[0 for x in range(limit+add)] for y in range(len(mtx))]
+        ext = 1
       counter += 1
       for i in range(0,b):
           for j in range(start,end):
-              newArray[m][n] = mtx[i][j]
-              n = n+1
-          m=m+1
+              newArray[i][n] = mtx[i][j]
+              n = n+1    
           n=0
       invPooled = np.array(LDA(newArray,mtx)) 
-      sparse(pos,invPooled,limit,sparseMat)
+      sparse(pos,invPooled,limit,sparseMat,ext,add)
       pos = pos + limit
-      m = 0
       start = end
       end = end + limit 
   originMTX = np.delete(mtx,a,axis=1)
@@ -118,9 +135,11 @@ def build_rotationtree_model(k):
 
 
 
-def sparse(pos,invPooled,limit,sparseMat):
+def sparse(pos,invPooled,limit,sparseMat,ext,add):
   e = pos
   f = pos
+  if(ext):
+    limit +=add
   for s in range(limit):
     for t in range(limit):
       sparseMat[e][f] = invPooled[s][t]
