d3024b3ecd83a6ff8384704892e418db63bf61d8
PS Narayanan <thepsnarayanan@gmail.com>
2018-02-03 20:20:55 +0530

    Bagged QDA

--- BAGGING/BaggingLDA.py
+++ BAGGING/BaggingLDA.py
@@ -44,15 +44,6 @@ def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	            trainingSet.append(dataset[x])
 
 
-
-
-def getAccuracy(testSet, predictions):
-	correct = 0
-	for x in range(len(testSet)):
-		if testSet[x][-1] == predictions[x]:
-			correct += 1
-	return (correct/float(len(testSet))) * 100.0
-
 main()	
 	
 
