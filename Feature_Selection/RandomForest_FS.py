8466fada6ba667d62754cdd9a42a31b260dd61b7
PSN <thepsnarayanan@gmail.com>
2018-12-29 13:00:56 +0530

    Generalising script

--- Feature_Selection/RandomForest_FS.py
+++ Feature_Selection/RandomForest_FS.py
@@ -4,18 +4,14 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectFromModel
 from sklearn.metrics import accuracy_score
+from numpy import loadtxt
 
 # Load the iris dataset
-iris = datasets.load_iris()
-
+dataset = loadtxt('../Dataset/bcancer.data', delimiter=",")
+# split data into X and y
+X = dataset[:,0:np.array(dataset).shape[1] - 1]
+y = dataset[:,np.array(dataset).shape[1] - 1]
 # Create a list of feature names
-feat_labels = ['Sepal Length','Sepal Width','Petal Length','Petal Width']
-
-# Create X from the features
-X = iris.data
-
-# Create y from output
-y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
 
@@ -25,20 +21,19 @@ clf = RandomForestClassifier(n_estimators=10000, random_state=0, n_jobs=-1)
 # Train the classifier
 clf.fit(X_train, y_train)
 
-# Print the name and gini importance of each feature
-for feature in zip(feat_labels, clf.feature_importances_):
+# Print the gini importance of each feature
+print("Gini Importance of each feature")
+print("-------------------------------")
+for feature in clf.feature_importances_:
     print(feature)
+print("-------------------------------")
 
 # Create a selector object that will use the random forest classifier to identify
-# features that have an importance of more than 0.15
-sfm = SelectFromModel(clf, threshold=0.15)
+sfm = SelectFromModel(clf, threshold=0.01)
 
 # Train the selector
 sfm.fit(X_train, y_train)
 
-# Print the names of the most important features
-for feature_list_index in sfm.get_support(indices=True):
-    print(feat_labels[feature_list_index])
 
 # Transform the data to create a new dataset containing only the most important features
 # Note: We have to apply the transform to both the training X and test X data.
