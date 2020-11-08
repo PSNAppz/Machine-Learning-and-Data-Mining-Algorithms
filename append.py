ada764758b095e443ce9398b04ba6bb1a2c5f67e
PS Narayanan <thepsnarayanan@gmail.com>
2020-11-08 12:55:45 +0530

    Dataset update

--- append.py
+++ append.py
@@ -1,9 +1,9 @@
 import csv
-filename = 'Dataset/ndrug/Morgan.csv'
-output = 'Dataset/ndrug/Morgan1.csv'
+filename = 'Dataset/drug/Toptor.csv'
+output = 'Dataset/drug/Toptor1.csv'
 
 with open(filename,'r') as csvinput:
     with open(output, 'w') as csvoutput:
         writer = csv.writer(csvoutput)
         for row in csv.reader(csvinput):
-            writer.writerow(row+['0'])
+            writer.writerow(row+['1'])
