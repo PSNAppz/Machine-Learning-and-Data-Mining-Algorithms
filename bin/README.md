4f3ccf4d1d80ef57c5c8312d282b9ab887c1843e
PS Narayanan <thepsnarayanan@gmail.com>
2021-05-09 18:20:24 +0530

    Update doc

---
+++ bin/README.md
@@ -0,0 +1,22 @@
+## How to setup the environment?
+
+### 1. Installing pre-requisite tools
+
+- `sudo apt-get update`
+- `sudo apt-get install python3`
+- `sudo apt install -y python3-pip`
+- `sudo apt-get install python-virtualenv`
+- `virtualenv venv --python=python3`
+
+### 2. Activating the virtualenv
+
+- `source venv/bin/activate`
+
+### 3. Running the algorithms
+
+- `python run.py`
+
+### 4. Changing the dataset in script
+
+- Specify the dataset name `DATA='sonar.data'` in run.py file.
+
--- bin/run.py
+++ bin/run.py
@@ -25,125 +25,125 @@ from models import (svm,\
                     bagged_knn
                 )
                     
+# Specify the dataset name below
+DATA='sonar.data'
+
+# calls SVM model
+print("SVM model : ")
+svm.call_function(DATA=DATA)
 
 div.divider()
 
-# calls SVM model
-# print("SVM model : ")
-# svm.call_function(DATA='sonar.data')
+print("XGBoost model : ")
+xgboost.call_function(DATA=DATA)
+
+div.divider()
+
+# calls random forest model
+print("Random Forest model : ")
+random_forest.call_function(DATA=DATA)
+
+div.divider()
+
+# calls random forest model
+print("QDA model : ")
+qda.call_function(DATA=DATA)
+
+div.divider()
+
+print("PCA & SVM model : ")
+pca_svm.call_function(DATA=DATA)
+
+div.divider()
+
+print("Naive Bayes model : ")
+naive_bayes.call_function(DATA=DATA)
+
+div.divider()
+
+# print("Naive model : ")
+# naive.call_function(DATA=DATA)
+
+div.divider()
+
+print("LDA Classifier model : ")
+lda_classifier.call_function(DATA=DATA)
+
+div.divider()
+
+# print("KMeans model : ")
+# kmeans.call_function(DATA=DATA)
 
 # div.divider()
 
-# print("XGBoost model : ")
-# xgboost.call_function(DATA='sonar.data')
+print("Decision Tree model : ")
+decisiontree.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# # calls random forest model
-# print("Random Forest model : ")
-# random_forest.call_function(DATA='sonar.data')
+print("Stacked model : ")
+stacking_scratch.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# # calls random forest model
-# print("QDA model : ")
-# qda.call_function(DATA='sonar.data')
+print("Stacked model : ")
+stacked_classifier.call_function(DATA=DATA)
 
-# div.divider()
-
-# print("PCA & SVM model : ")
-# pca_svm.call_function(DATA='sonar.data')
-
-# div.divider()
-
-# print("Naive Bayes model : ")
-# naive_bayes.call_function(DATA='sonar.data')
-
-# div.divider()
-
-# # print("Naive model : ")
-# # naive.call_function(DATA='sonar.data')
-
-# div.divider()
-
-# print("LDA Classifier model : ")
-# lda_classifier.call_function(DATA='sonar.data')
-
-# div.divider()
-
-# # print("KMeans model : ")
-# # kmeans.call_function(DATA='sonar.data')
-
-# # div.divider()
-
-# print("Decision Tree model : ")
-# decisiontree.call_function(DATA='sonar.data')
-
-# div.divider()
-
-# print("Stacked model : ")
-# stacking_scratch.call_function(DATA='sonar.data')
-
-# div.divider()
-
-# print("Stacked model : ")
-# stacked_classifier.call_function(DATA='sonar.data')
-
-# div.divider()
+div.divider()
 
 print("Rotation Forest PCA model : ")
-rotation_forest_pca.call_function(DATA='sonar.data')
+rotation_forest_pca.call_function(DATA=DATA)
 
 div.divider()
 
 print("Rotation Forest LDA model : ")
-rotation_forest_lda.call_function(DATA='sonar.data')
+rotation_forest_lda.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("KNN model : ")
-# knn.call_function(DATA='sonar.data')
+print("KNN model : ")
+knn.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("Feature Selection RFE model : ")
-# rfe.call_function(DATA='sonar.data')
+print("Feature Selection RFE model : ")
+rfe.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("Feature Selection Random Forest model : ")
-# randomforest_fs.call_function(DATA='sonar.data')
+print("Feature Selection Random Forest model : ")
+randomforest_fs.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("Feature Selection BPSO model : ")
-# bpso.call_function(DATA='sonar.data')
+print("Feature Selection BPSO model : ")
+bpso.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("Gradient Boosting model : ")
-# gradient_boosting.call_function(DATA='sonar.data')
+print("Gradient Boosting model : ")
+gradient_boosting.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("AdaBoosting model : ")
-# ada_boosting.call_function(DATA='sonar.data')
+print("AdaBoosting model : ")
+ada_boosting.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("Bagged SVM model : ")
-# bagged_svm.call_function(DATA='sonar.data')
+print("Bagged SVM model : ")
+bagged_svm.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("Bagged QDA model : ")
-# bagged_qda.call_function(DATA='sonar.data')
+print("Bagged QDA model : ")
+bagged_qda.call_function(DATA=DATA)
 
-# div.divider()
+div.divider()
 
-# print("Bagged LDA model : ")
-# bagged_lda.call_function(DATA='sonar.data')
-# div.divider()
+print("Bagged LDA model : ")
+bagged_lda.call_function(DATA=DATA)
+div.divider()
 
-# print("Bagged KNN model : ")
-# bagged_knn.call_function(DATA='sonar.data')
+print("Bagged KNN model : ")
+bagged_knn.call_function(DATA=DATA)
